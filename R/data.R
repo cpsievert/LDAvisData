77dd42fcf9d2d799e48479fb387db0296d548511
Anonymous@example.com <cpsievert1@gmail.com>
2014-12-11 11:31:37 -0600

    update docs

--- R/data.R
+++ R/data.R
@@ -1,10 +1,13 @@
 #' Movie Review Data
 #'
-#' @references http://www.cs.cornell.edu/people/pabo/movie-review-data/
+#' Movie reviews that are often used in sentiment analysis. Note this is v2.0
+#' of the polarity data.
 #'
 #' @docType data
 #' @name reviews
-#' @format A list with element containing a review. The element name can be used to identify
-#' the review 'number' and whether the review was positive/negative.
-#' 
+#' @format A character vector where each element contains a review.
+#' The element name can be used to identify the review 'number'
+#' and whether the review was positive/negative.
+#' @source \url{http://www.cs.cornell.edu/people/pabo/movie-review-data/}
+#'
 NULL
